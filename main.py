b13b26f6ebdbf47ff6a8493c6c556f229e26d627
adrivrie <adriaan.de.vries@hotmail.com>
2019-09-16 11:48:13 +0200

    added counter and bugfix

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ done = set()
 latin = set()
 for _ in range(len(data.paragraphs)):
     batch = data.get_next_batch(1)
-    if batch[0][1] in done:
+    if batch[1][0] in done:
         continue
     print(batch[0][0])
     print(batch[1][0])
+    print(len(done), '/', len(data.languages))
     i = 'c'
     while i[0].lower() not in 'yn':
         print("latin? (y/n)")
